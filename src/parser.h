e0511d09d05cc90ad3d2599d28d2402ca28b9c40
Arc676/Alessandro Vinciguerra <alesvinciguerra@gmail.com>
2018-03-06 14:31:50 +0800

    Commented parser header

--- src/parser.h
+++ src/parser.h
@@ -20,25 +20,87 @@
 
 #include "lexer.h"
 
+/**
+ * Top-level parse function to parse the whole script
+ * @param fp FILE* from which to read
+ * @return program token containing the script
+ */
 Token* parseTopLevel(FILE* fp);
 
+/**
+ * Parse a function from the token stream
+ * @param fp FILE* from which to read
+ * @return function token containing the defined function
+ */
 Token* parseFunction(FILE* fp);
 
+/**
+ * Parse an if-block from the token stream
+ * @param fp FILE* from which to read
+ * @return conditional token defining the block
+ */
 Token* parseIf(FILE* fp);
 
+/**
+ * Parse a function call from the token stream
+ * @param fp FILE* from which to read
+ * @return call token indicating the called function and passed arguments
+ */
 Token* parseCall(FILE* fp);
 
+/**
+ * Parse depending on the current token
+ * @param fp FILE* from which to read
+ * @return token returned by the called delegate function
+ */
 Token* parseAtom(FILE* fp);
 
+/**
+ * Parse a sequence of statements
+ * @param fp FILE* from which to read
+ * @return program token containing statements
+ */
 Token* parseProg(FILE* fp);
 
-Token* parseIdentifier(FILE* fp);
+/**
+ * Parse the token stream for a variable name
+ * @param fp FILE* from which to read
+ * @return variable token
+ */
+Token* parseVariableName(FILE* fp);
 
+/**
+ * Parse the next expression contained in the token stream
+ * @param fp FILE* from which to read
+ * @return token representing the next expression in the stream
+ */
 Token* parseExpression(FILE* fp);
 
+/**
+ * Parse the stream for a function call or identifier
+ * @param fp FILE* from which to read
+ * @return the corresponding token
+ */
 Token* potentialCall(FILE* fp);
 
-Token* potentialBinary(FILE* fp);
+/**
+ * Parse the stream for a potential binary expression
+ * @param fp FILE* from which to read
+ * @param token given token that might start an expression
+ * @param prec precedence of the given token
+ * @return the given token or another token representing the expression
+ */
+Token* potentialBinary(FILE* fp, Token* token, int prec);
 
+/**
+ * Parse the stream for a list of tokens between a specified start and end
+ * characters delimited by the specified separator
+ * @param fp FILE* from which to read
+ * @param start starting character for the list
+ * @param end ending character for the list
+ * @param sep delimiter character for the list
+ * @param parse parse function to parse the delimited tokens
+ * @return an array containing the parsed tokens
+ */
 Token** parseDelimited(FILE* fp, char start, char end, char sep,
 	Token* (*parse)(FILE*));
